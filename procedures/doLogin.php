9e240d77bb216cad1cef8d448e78c62b60febd02
Melinda S <roseofgold@gmail.com>
2019-06-04 21:22:43 -0700

    log in, out, and with registration

--- procedures/doLogin.php
+++ procedures/doLogin.php
@@ -15,9 +15,5 @@ if (!password_verify(request()->get('password'),$user['password']))
     redirect('/login.php');
 }
 
-$session->set('auth_logged_in',true);
-$session->set('auth_user_id',(int) $user['id']);
-$session->set('auth_roles',(int) $user['role_id']);
-
-$session->getFlashBag()->add('success', 'Successfully Logged In');
+saveUserSession($user);
 redirect('/');
--- procedures/doRegister.php
+++ procedures/doRegister.php
@@ -18,5 +18,6 @@ if(!empty($user)){
 
 $hashed = password_hash($password,PASSWORD_DEFAULT);
 $user = createUser($username,$hashed);
+saveUserSession($user);
 $session->getFlashBag()->add('success','User Added');
 redirect('/');
